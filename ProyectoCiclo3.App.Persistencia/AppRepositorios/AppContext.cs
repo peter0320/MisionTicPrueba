5114b76cb53b914a87cda79319c29277ca7cde19
peter0320 <peyinos45@gmail.com>
2021-09-21 20:38:17 -0500

    commit

---
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,16 @@
+
+//genera la conexion con la base de datos
+using Microsoft.EntityFrameworkCore;
+using ProyectoCiclo3.App.Dominio;
+ 
+namespace ProyectoCiclo3.App.Persistencia
+{
+    public class AppContext: DbContext{
+        public DbSet<Encomienda> Encomiendas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            if(!optionsBuilder.IsConfigured){
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ProyectoCiclo3");
+            }
+        }
+    }
+}
