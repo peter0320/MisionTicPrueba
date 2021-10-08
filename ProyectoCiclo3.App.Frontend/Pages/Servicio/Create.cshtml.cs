21a430e4d9be7d12704ebefb877c4506ed223a16
peter0320 <peyinos45@gmail.com>
2021-10-08 18:46:12 -0500

    Modulo_Servicios

---
+++ ProyectoCiclo3.App.Frontend/Pages/Servicio/Create.cshtml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
+using ProyectoCiclo3.App.Dominio;
+ 
+namespace ProyectoCiclo3.App.Frontend.Pages
+{
+    public class FormServicioModel : PageModel
+    {
+ 
+        private readonly RepositorioServicio repositorioServicio;
+        private readonly RepositorioUsuario repositorioUsuario;
+        private readonly RepositorioEncomienda repositorioEncomienda; 
+        [BindProperty]
+        public Servicio Servicio { get; set; }
+        public IEnumerable<Usuario> Usuarios {get;set;}
+        public IEnumerable<Encomienda> Encomiendas {get;set;}  
+
+      
+ 
+        public FormServicioModel(RepositorioServicio repositorioServicio,RepositorioUsuario repositorioUsuario, RepositorioEncomienda repositorioEncomienda)
+       {
+            this.repositorioServicio=repositorioServicio;
+            this.repositorioUsuario=repositorioUsuario;
+            this.repositorioEncomienda=repositorioEncomienda;
+
+       }
+
+       public void OnGet()
+    {
+        Usuarios=repositorioUsuario.GetAll();
+        Encomiendas=repositorioEncomienda.GetAll();
+    }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }            
+           Servicio = repositorioServicio.Create(Servicio);            
+            return RedirectToPage("./List");
+        }
+    }
+}
---
+++ ProyectoCiclo3.App.Frontend/Pages/Servicio/Edit.cshtml.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
+using ProyectoCiclo3.App.Dominio;
+ 
+namespace ProyectoCiclo3.App.Frontend.Pages
+{
+    public class EditServicioModel : PageModel
+    {
+        private readonly RepositorioServicio repositorioServicio;
+        private readonly RepositorioUsuario repositorioUsuario;
+        private readonly RepositorioEncomienda repositorioEncomienda; 
+        [BindProperty]        
+        public Servicio Services {get;set;}
+        public IEnumerable<Usuario> Usuarios {get;set;}
+        public IEnumerable<Encomienda> Encomiendas {get;set;}  
+ 
+ 
+        public EditServicioModel(RepositorioServicio repositorioServicio, RepositorioUsuario repositorioUsuario, RepositorioEncomienda repositorioEncomienda)
+       {
+            this.repositorioServicio=repositorioServicio;
+            this.repositorioUsuario=repositorioUsuario;
+            this.repositorioEncomienda=repositorioEncomienda;
+       }
+
+            
+             /* 
+            public void OnGet()
+             {
+                Usuarios=repositorioUsuario.GetAll();
+                Encomiendas=repositorioEncomienda.GetAll();
+                
+             } */
+
+        public IActionResult OnGet(int servicioId)
+        {
+                          
+            Services=repositorioServicio.GetServicioWithId(servicioId);
+            return Page();
+ 
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if(Services.id>0)
+            {
+            Services = repositorioServicio.Update(Services);
+            }
+            return RedirectToPage("./List");
+       }
+    }
+}
